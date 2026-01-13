db5154ddea6cbc921a7b9a213a803ab55ec2520a
theChebster <thechebsticentity@gmail.com>
2026-01-13 12:40:12 +0700

    Simplify RootLayout function syntax

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,16 +9,10 @@ export const metadata: Metadata = {
   description: "Innovative Tech Solutions",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
